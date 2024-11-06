a4d47c4aad08459bae211c8017c9801f3ea25d35
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-11-06 13:07:46 +0100

    UPDATE: Loading images from API

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const photosURL = "https://jsonplaceholder.typicode.com/photos?_limit=6";
 axios
   .get(photosURL)
   .then((res) => {
-    console.log(res);
+    //console.log(res);
     const posts = res.data
 
     appendposts(posts,cardContainer)
@@ -15,14 +15,25 @@ axios
     console.log(err);
   });
 function appendposts(posts, root){
-    console.log(posts, root)
+    //console.log(posts, root)
 
 posts.forEach((post) => {
-    console.log(post)
+    //console.log(post)
     
     const title = post.title
     const photo = post.url
 
-    console.log(title, photo)
+    //console.log(title, photo)
+
+    const postCardHTML = `<div class="col-12 col-sm-6 col-lg-4">
+      <div class="card h-100">
+        <div class="card-body">
+          <div class="photo-placeholder" style="background-image: url('${photo}'); height: 300px;"></div>
+          <h5 class="card-title mt-3">${title}</h5>
+        </div>
+      </div>
+    </div>`;
+
+    cardContainer.innerHTML += postCardHTML;
 })
 }

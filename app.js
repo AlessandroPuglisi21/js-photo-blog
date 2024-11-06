a34749c27e7ae36e1b6eaa8ee03083d68cfa4fed
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-11-06 12:55:47 +0100

    ADD: work in progress js Axios

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('test')
+console.log("test");
+
+const cardContainer = document.getElementById("card-container");
+const photosURL = "https://jsonplaceholder.typicode.com/photos?_limit=6";
+
+axios
+  .get(photosURL)
+  .then((res) => {
+    console.log(res);
+    const posts = res.data
+
+    appendposts(posts,cardContainer)
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+function appendposts(posts, root){
+    console.log(posts, root)
+
+posts.forEach((post) => {
+    console.log(post)
+    
+    const title = post.title
+    const photo = post.url
+
+    console.log(title, photo)
+})
+}

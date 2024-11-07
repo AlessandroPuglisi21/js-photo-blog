e41b785864a69fbc5250518faf112857bfbb8b03
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-11-07 14:03:37 +0100

    ADD: photo in the overlay

--- app.js
+++ app.js
@@ -2,59 +2,55 @@ console.log("test");
 
 //DEFINIZIONE DI DOVE LA CARD VERRÁ AGGIUNTA IN MODO DINAMICO E URL DELL'API
 const cardContainer = document.getElementById("card-container");
-const photosURL = "https://jsonplaceholder.typicode.com/photos?_limit=6";
+const photosURL = "https://jsonplaceholder.typicode.com/photos?_limit=12";
 
 //RICHIESTA AXIOS
 axios
   .get(photosURL)
   .then((res) => {
-    //console.log(res);
-    const posts = res.data; //! ===> ESTRAGGO I DATI DALLA RISPOSTA API
-
-    appendposts(posts, cardContainer); //! ===> CHIAMO UNA FUNZIONE E LE PASSO I DATI DI 'posts' CHE POII ANDRANNO NELLA 'cardContainer'
+    const posts = res.data; // ESTRAGGO I DATI DALLA RISPOSTA API
+    appendposts(posts, cardContainer); // CHIAMO UNA FUNZIONE E LE PASSO I DATI DI 'posts'
   })
   .catch((err) => {
     console.log(err);
   });
 
-// CREO LA FUNZIONE
-
+// CREO LA FUNZIONE PER AGGIUNGERE LE CARD
 function appendposts(posts, cardContainer) {
-  //console.log(posts, root)
-
   posts.forEach((post) => {
-    //! ===> CREO LE ITERAZIONI PER OGNI OGGETTO NELL'ARRAY 'posts'
-    //console.log(post)
-
-    const title = post.title; //! ===> ESTRAZIONE DEL TITOLO E DELL'URL DELLA FOTO
+    const title = post.title; // ESTRAZIONE DEL TITOLO E DELL'URL DELLA FOTO
     const photo = post.url;
 
-    //console.log(title, photo)
-
-    //CREAZIONE DELL'HTML CHE VERRÁ AGGIUNTO
-
+    // CREAZIONE DELL'HTML DELLA CARD
     const postCardHTML = `<div class="col-12 col-sm-12 col-lg-6 col-xl-4 card-hover" style="position: relative;">
-  <div class="pin">
-    <img src="./img/pin.svg" alt="">
-  </div>
-  <div class="card h-100">
-    <div class="card-body">
-      <div class="photo-placeholder" id="test" style="background-image: url('${photo}');"></div>
-      <h5 class="card-title mt-3 text-center fs-4">"${title}" </h5>
-    </div>
-  </div>
-</div>
-`;
+      <div class="pin">
+        <img src="./img/pin.svg" alt="">
+      </div>
+      <div class="card h-100">
+        <div class="card-body">
+          <div class="photo-placeholder" style="background-image: url('${photo}');"></div>
+          <h5 class="card-title mt-3 text-center fs-4">"${title}"</h5>
+        </div>
+      </div>
+    </div>`;
 
     // AGGIUNGO LA CARD AL CONTENITORE
     cardContainer.innerHTML += postCardHTML;
   });
 
   // Aggiungo l'evento click alle card dopo che sono state aggiunte al DOM
-  const cards = document.querySelectorAll(".card-body"); 
+  const cards = document.querySelectorAll(".card-body");
 
-  cards.forEach(function(card) {
+  cards.forEach(function (card) {
     card.addEventListener("click", function () {
+      // OTTENGO L'IMMAGINE DELLA CARD SELEZIONATAA
+      const photo = card.querySelector(".photo-placeholder").style.backgroundImage.slice(5, -2); 
+
+      // IMPOSTO L'IMMAGINE CHE VERRÁ VISUALIZZATA NELL'OVERLAY
+      const overlayImg = document.getElementById("overlay-img");
+      overlayImg.src = photo;
+
+      // MOSTRA L'OVERLAY
       const overlay = document.getElementById("overlay");
       overlay.classList.remove("d-none");
       overlay.classList.add("d-block");
@@ -68,3 +64,4 @@ const overlay = document.getElementById("overlay");
 document.getElementById("closing-button").addEventListener("click", function () {
   overlay.classList.add("d-none");
 });
+
